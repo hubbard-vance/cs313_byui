5365a0b5f6cab4b5c0ebe1347332843d60338db3
hubbard-vance <hubbard-vance@users.noreply.github.com>
2017-09-20 21:56:24 -0600

    Add files via upload

---
+++ web/bananas.js
@@ -0,0 +1,11 @@
+
+function myClicker() {
+    alert("Clicked");
+}
+function changeDivColor() {
+	var newcolor= document.getElementById("colorChange").value;
+	document.getElementById("firstDiv").style.backgroundColor = newcolor;
+	
+	
+}
+
---
+++ web/teamwk2_vancehubbard.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Team Week2 Assignment</title>
+<link rel="stylesheet" href="grapes.css">
+<script src="bananas.js"></script>
+
+
+</head>
+
+<body>
+<div class="Team2Divs" id="firstDiv">
+<h1>Welcome to Team Assignment for Week2 </h1>
+<h2>CS313 BYU-Idaho </h2>
+<p>First Div</p>
+<form>
+ <input type="text" id="colorChange" name="colorChange" size="80px">
+ <button type="button" onclick="changeDivColor()">Change Color</button>
+</form>
+
+</div>
+
+<div class="Team2Divs">
+<h1>Welcome to Team Assignment for Week2 </h1>
+<h2>CS313 BYU-Idaho </h2>
+<p>Second Div</p>
+
+</div>
+
+<div class="Team2Divs">
+<h1>Welcome to Team Assignment for Week2 </h1>
+<h2>CS313 BYU-Idaho </h2>
+<p>Third Div</p>
+<button onclick="myClicker()"> Click Me </button>
+</div>
+
+</body>
+</html> 
