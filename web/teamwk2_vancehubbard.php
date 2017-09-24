d7a8b354c1651cbabf9dc99f9cccea8301bb6ff3
hubbard-vance <hubbard-vance@users.noreply.github.com>
2017-09-23 21:09:23 -0600

    Update teamwk2_vancehubbard.php

--- web/teamwk2_vancehubbard.php
+++ web/teamwk2_vancehubbard.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -36,43 +35,4 @@
 </div>
 
 </body>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-<title>Team Week2 Assignment</title>
-<link rel="stylesheet" href="grapes.css">
-<script src="bananas.js"></script>
-
-
-</head>
-
-<body>
-<div class="Team2Divs" id="firstDiv">
-<h1>Welcome to Team Assignment for Week2 </h1>
-<h2>CS313 BYU-Idaho </h2>
-<p>First Div</p>
-<form>
- <input type="text" id="colorChange" name="colorChange" size="80px">
- <button type="button" onclick="changeDivColor()">Change Color</button>
-</form>
-
-</div>
-
-<div class="Team2Divs">
-<h1>Welcome to Team Assignment for Week2 </h1>
-<h2>CS313 BYU-Idaho </h2>
-<p>Second Div</p>
-
-</div>
-
-<div class="Team2Divs">
-<h1>Welcome to Team Assignment for Week2 </h1>
-<h2>CS313 BYU-Idaho </h2>
-<p>Third Div</p>
-<button onclick="myClicker()"> Click Me </button>
-</div>
-
-</body>
->>>>>>> 5365a0b5f6cab4b5c0ebe1347332843d60338db3
 </html> 
